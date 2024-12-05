15977f55afd636f76ecf56345585340c5f02bcdb
yuousef <yosofmomeni111@gmail.com>
2024-12-05 19:56:04 +0330

    memoi

--- Untitled-1.php
+++ Untitled-1.php
@@ -6,6 +6,22 @@ function factorial($number){
     return $number * factorial($number -1);
     }
 
+    function factorialMemo($number){
+        static $arr =[];
+
+        if ($number == 0 || $number==1)
+        return 1;
+       
+        if(!empty($arr[$number]))
+            return $arr[$number];
+    
+            ////////
+       $ret = $number * factorialMemo($number -1);
+
+       $arr[$number] = $ret;
+    
+        return $ret;
+        }
     
     $time = microtime(TRUE);
 for ($num1=1;$num1<=1000;$num1++){
